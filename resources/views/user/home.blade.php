da22b2e31a4d40c9d5661986a4cff09ab8cf2f3c
Kmpg <kmyat5258@gmail.com>
2022-07-30 17:49:38 +0630

    Implement home page design (work-in-progress)

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,9 +1,9 @@
 @extends('user.master.master')
 @section('title')
-    Home
+Home
 @endsection
 @section('home-active')
-    active
+active
 @endsection
 @section('content')
 <header class="header">
@@ -21,7 +21,104 @@
         </div>
     </div>
 </header>
-  <footer>
+
+<style>
+    .nav-tabs .nav-item.show .nav-link,
+    .nav-tabs .nav-link.active {
+        background: transparent !important;
+        color: black !important;
+        border-bottom: 2px solid black !important;
+        border-radius: 0px !important;
+        border-color: #f2f2f2;
+    }
+
+    .nav-item {
+        padding: 0 20px;
+    }
+</style>
+
+<body class="body">
+
+    <div class="container mt-2">
+        <h3 class="mb-3 fw-bolder text-center">Our Favourites</h3>
+
+
+        <div style="justify-content: center;" class="mb-3">
+
+
+            <!-- Tabs navs -->
+            <ul class="nav nav-tabs nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="ex1-tab-1" data-bs-toggle="tab" href="#ex1-tabs-1" role="tab" aria-controls="ex1-tabs-1" aria-selected="true">FOR RUNNING</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="ex1-tab-2" data-bs-toggle="tab" href="#ex1-tabs-2" role="tab" aria-controls="ex1-tabs-2" aria-selected="false">FOR EVERYDAY</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="ex1-tab-3" data-bs-toggle="tab" href="#ex1-tabs-3" role="tab" aria-controls="ex1-tabs-3" aria-selected="false">FOR SUMMER</a>
+                </li>
+            </ul>
+            <!-- Tabs navs -->
+
+            <!-- Tabs content -->
+            <div class="tab-content" id="ex1-content">
+                <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel" aria-labelledby="ex1-tab-1">
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass1.jpeg')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass1.jpeg')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass1.jpeg')}}" class="img-thumbnail">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass2.jpg')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass2.jpg')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass2.jpg')}}" class="img-thumbnail">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass3.webp')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass3.webp')}}" class="img-thumbnail">
+                        </div>
+                        <div class="col-md-4">
+                            <img src="{{asset('images/template/ass3.webp')}}" class="img-thumbnail">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- Tabs content -->
+
+
+
+
+
+        </div>
+
+    </div>
+
+</body>
+<footer>
     <div>
         <div>
 
